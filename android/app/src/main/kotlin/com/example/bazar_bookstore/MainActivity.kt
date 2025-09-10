7ee835636c530558fb21459fe4a532efa7c58fff
zitounirania <mr_zitouni@esi.dz>
2025-09-10 11:22:50 +0100

    add : flutter + supabase + riverpod setup

---
+++ android/app/src/main/kotlin/com/example/bazar_bookstore/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bazar_bookstore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/providers/supabase_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final supabaseClientProvider = Provider<SupabaseClient>((ref) {
+  return Supabase.instance.client;
+});
+
+//run ref.watch(supabaseClientProvider) to get the client 
